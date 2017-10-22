9dd5398fe65cb4bad63cd79947af26f8653683d9
seun9su <seun9su@gmail.com>
2017-10-22 11:13:05 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 # Use: route, awk, ping
 ### For Linux ###
 ## get default gateway by route
-# gateway=$(route -n | awk '$1 == "0.0.0.0" {print $2}')
+gateway=$(route -n | awk '$1 == "0.0.0.0" {print $2}')
 ## ping to default gateway
-# ping -c $gateway > /dev/null 2>&1 # -c option for 1 time ping
+ping -c $gateway > /dev/null 2>&1 # -c option for 1 time ping
 ### For Mac ###
-gateway=$(netstat -nr | awk '$1 == "default" {print $2}') 
+#gateway=$(netstat -nr | awk '$1 == "default" {print $2}') 
 # -r option: current path table
 # -n option: show IP, not hostname
 ping -c 1 $gateway > /dev/null 2>&1
