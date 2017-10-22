58074c3879bdccb2778939af9799cb15da2f5f7c
seun9su <seun9su@gmail.com>
2017-10-22 14:48:42 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,16 +1,13 @@
-# Use: ifconfig, awk
-# 사용처: 서버 NIC와 IP 주소 목록을 표시하고 싶을 때
-# 실행 예제
-# ./nic-ipaddr.sh
-
-## for Linux
-# ifconfig 명령어로 유효한 인터페이스 표시
-# awk 명령어로 인터페이스명과 IP 주소 추출
-LANG=C /sbin/ifconfig |\
-awk '/^[a-z]/ {print "[" $1 "]"}
-/inet / {split($2,arr,":"); print arr[2]}'
-
-## for Mac/FreeBSD
-# LANG=C /sbin/ifconfig |\
-# awk '/^[a-z]/ {print "[" $1 "]"}
-# /inet / {print $2}'
+commname="/usr/sbin/httpd"
+## Linux
+start="service httpd start"
+# Mac/FreeBSD
+start="/usr/sbin/apachectl start"
+# 감시 대상 명령어 프로세스 수 카운트
+count=$(ps ax -o command | grep "$commname" | grep -v "^grep" | wc -l)
+if [ "$count" -eq 0 ]; then
+  date_str=$(date '+%Y/%m/%d %H:%M:%S')
+  echo "[$date_str] Can't find process $commname." >&2
+  echo "[$date_str] Execute process $commname." >&2
+  $start
+fi
