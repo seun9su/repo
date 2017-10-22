4ebcb4b7e99cd435fb0a9f7647158e41c029ebd4
seun9su <seun9su@gmail.com>
2017-10-22 14:22:17 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,18 +1,16 @@
-#!/bin/sh
-# Use: route, awk, ping
-### For Linux ###
-## get default gateway by route
-gateway=$(route -n | awk '$1 == "0.0.0.0" {print $2}')
-## ping to default gateway
-ping -c $gateway > /dev/null 2>&1 # -c option for 1 time ping
-### For Mac ###
-gateway=$(netstat -nr | awk '$1 == "default" {print $2}') 
-# -r option: current path table
-# -n option: show IP, not hostname
-ping -c 1 $gateway > /dev/null 2>&1
-## check ping end status
-if [ $? -eq 0 ]; then
-  echo "[Success] ping -> $gateway"
-else
-  echo "[Failed] ping -> $gateway"
-fi
+# Use: ifconfig, awk
+# 사용처: 서버 NIC와 IP 주소 목록을 표시하고 싶을 때
+# 실행 예제
+# ./nic-ipaddr.sh
+
+## for Linux
+# ifconfig 명령어로 유효한 인터페이스 표시
+# awk 명령어로 인터페이스명과 IP 주소 추출
+LANG=C /sbin/ifconfig |\
+awk '/^[a-z]/ {print "[" $1 "]"}
+/inet / {split($2,arr,":"); print arr[2]}'
+
+## for Mac/FreeBSD
+# LANG=C /sbin/ifconfig |\
+# awk '/^[a-z]/ {print "[" $1 "]"}
+# /inet / {print $2}'
